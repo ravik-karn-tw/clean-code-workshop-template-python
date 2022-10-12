fc7640519c3b6975beb7e5a75dbc043ad099e95c
ravik-karn-tw <60911433+ravik-karn-tw@users.noreply.github.com>
2022-10-12 10:14:02 +0530

    Initial commit

---
+++ src/movie_rental/Rental.py
@@ -0,0 +1,13 @@
+from src.movie_rental.Movie import Movie
+
+
+class Rental:
+    def __init__(self, movie: Movie, days_rented: int):
+        self.movie = movie
+        self.days_rented = days_rented
+
+    def get_days_rented(self) -> int:
+        return self.days_rented
+
+    def get_movie(self) -> Movie:
+        return self.movie
